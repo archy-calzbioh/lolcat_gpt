a521e3f18e4f15213ed47a1043bbf17cc719ff9c
king-archy <king.archy.13013@gmail.com>
2023-04-07 12:22:55 -0700

    allows for editing answer on frontend

--- src/App.js
+++ src/App.js
@@ -6,102 +6,131 @@ import axios from 'axios'
 
 
 const App = () => {
+  // STATE
+  const [newQuestion, setNewQuestion] = useState("");
+  const [gptArray, setGptArray] = useState([]);
 
-    // STATE
-    const [newQuestion, setNewQuestion] = useState('')
-    const [gptArray, setGptArray] = useState([])
+  // EDITING ANSWERS STATE 
+  const [isEditing, setIsEditing] = useState({});
+  const [editedAnswer, setEditedAnswer] = useState({});
 
+  // EDIT ANSWER HANDLERS
 
+  const editAnswer = (index) => {
+    setIsEditing({ ...isEditing, [index]: true });
+  };
 
-    // HANDLERS
+  const saveEditedAnswer = (index) => {
+    // Update the answer in gptArray (and optionally update the backend server)
+    const updatedGptArray = gptArray.map((gpt, i) => {
+      if (i === index) {
+        return { ...gpt, answer: editedAnswer[index] };
+      }
+      return gpt;
+    });
+    setGptArray(updatedGptArray);
+    setIsEditing({ ...isEditing, [index]: false });
+  };
 
-    //handleChange
-    const handleChange = (e) => {
-        setNewQuestion(e.target.value);
-        console.log(newQuestion);
-    }
+  // HANDLERS
 
-    // handleSubmit
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		console.log(newQuestion)
-		axios.post('http://localhost:3080/gpt', 
-		{
-			question: newQuestion 
-		}
-		).then((response) => {
-			axios
-			.get('http://localhost:3080/gpt')
-			.then((response)=>{
-				setGptArray(response.data)
-			})
-		})
-	}
+  //handleChange
+  const handleChange = (e) => {
+    setNewQuestion(e.target.value);
+    console.log(newQuestion);
+  };
 
-
-    	
-
-
-
-
-
-    // THE getData() Function
-     // Retrieves all the data in the DB
-    // Fills our Array with Data Objects
-
-    const getData = () => {
-      axios.get("http://localhost:3080/gpt").then((response) => {
-        setGptArray(response.data);
+  // handleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newQuestion);
+    axios
+      .post("http://localhost:3080/gpt", {
+        question: newQuestion,
+      })
+      .then((response) => {
+        axios.get("http://localhost:3080/gpt").then((response) => {
+          setGptArray(response.data);
+        });
       });
-    };
+  };
 
+  // THE getData() Function
+  // Retrieves all the data in the DB
+  // Fills our Array with Data Objects
 
+  const getData = () => {
+    axios.get("http://localhost:3080/gpt").then((response) => {
+      setGptArray(response.data);
+    });
+  };
 
-    // USE EFFECT
-    // Have GPT Array in the Dependency Array so when it is updated, callAPI() is called and page refreshes!!
+  // USE EFFECT
+  // Have GPT Array in the Dependency Array so when it is updated, callAPI() is called and page refreshes!!
 
-     useEffect(() => {
-       getData();
-     }, [gptArray]);
+  useEffect(() => {
+    getData();
+  }, [gptArray]);
 
-
-    return (
-        <div className='App'>
-            {/* SIDEBAR - Questions and Answers */}
-            <aside className='sidemenu'>
-                <h1 className="title">Q&A</h1>
-                <hr />
+  return (
+    <div className="App">
+      {/* SIDEBAR - Questions and Answers */}
+      <aside className="sidemenu">
+        <h1 className="title">Q&A</h1>
+        <hr />
+        <section>
+          {/* Mapping through Array to display Questions and Answers  */}
+          {gptArray.map((gpt, i) => {
+            return (
+              <>
                 <section>
-                    {/* Mapping through Array to display Questions and Answers  */}
-                     {
-                        gptArray.map((gpt, i) => {
-                            return (
-                            <>
-                            <section>
-                                <p className="question" key={i}>{gpt.question}</p>
-                            </section>
-                            <section>
-                                <p key={i}>{gpt.answer}</p>
-                            </section>
-                            <hr />
-                            </>
-                            )
-                        })
-                    }
+                  <p className="question" key={i}>
+                    {gpt.question}
+                  </p>
                 </section>
-            </aside>
-            {/* BODY */}
-            <section className='chatbox'>
-                <div className='chat-input-holder'>
-                    {/* Our Question Input Field --- All to be asked will go here..... */}
-                    <form onSubmit={handleSubmit}>
-                        <input onChange={handleChange} rows='1' className="chat-input-textarea"></input>
-                    </form>
-                </div>
-            </section>
-        </div>
+                <section>
+                  {isEditing[i] ? (
+                    <>
+                      <input
+                        value={editedAnswer[i] || gpt.answer}
+                        onChange={(e) =>
+                          setEditedAnswer({
+                            ...editedAnswer,
+                            [i]: e.target.value,
+                          })
+                        }
+                      />
+                      <button onClick={() => saveEditedAnswer(i)}>Save</button>
+                    </>
+                  ) : (
+                    <>
+                      <p key={i}>{gpt.answer}</p>
+                      <button onClick={() => editAnswer(i)}>Edit</button>
+                    </>
+                  )}
+                </section>
 
-    )
+                <hr />
+              </>
+            );
+          })}
+        </section>
+      </aside>
+      {/* BODY */}
+      <section className="chatbox">
+        <div className="chat-input-holder">
+          {/* Our Question Input Field --- All to be asked will go here..... */}
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={handleChange}
+              rows="1"
+              className="chat-input-textarea"
+            ></input>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App
