beef67af0cc7fa361027b05fc5dfcc86351d9335
king-archy <king.archy.13013@gmail.com>
2023-04-17 08:44:41 -0700

    code is commented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+// Importing required dependencies and components
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { CopyBlock, dracula } from "react-code-blocks";
 
-
-
-
-
-
+// MyCoolCodeBlock component to display the code blocks with syntax highlighting
 function MyCoolCodeBlock({
   code,
   language,
@@ -28,39 +25,39 @@ function MyCoolCodeBlock({
   );
 }
 
- 
-
-
+// Main App component
 const App = () => {
-  // Create a new state to store the generated image URL
+  // State for storing the generated image URL
   const [generatedImageUrl, setGeneratedImageUrl] = useState(null);
 
-  //select an element in the sidebar
+  // State for selecting an element in the sidebar
   const [selectedElementId, setSelectedElementId] = useState(null);
 
   // Handler function for when an element in the sidebar is clicked
-const handleElementClick = (elementId) => {
-  const selectedElement = gptArray.find((gpt) => gpt._id === elementId);
-  setSelectedCodeBlock(selectedElement.answer);
-};
+  const handleElementClick = (elementId) => {
+    const selectedElement = gptArray.find((gpt) => gpt._id === elementId);
+    setSelectedCodeBlock(selectedElement.answer);
+  };
 
-
-  //add a new state to store the selected code block
+  // State for storing the selected code block
   const [selectedCodeBlock, setSelectedCodeBlock] = useState(null);
 
-  // STATE
+  // State for handling the new question input
   const [newQuestion, setNewQuestion] = useState("");
+
+  // State for storing the GPT-3 generated answers array
   const [gptArray, setGptArray] = useState([]);
 
-  // EDITING ANSWERS STATE
+  // State and handlers for editing the answers
   const [isEditing, setIsEditing] = useState({});
   const [editedAnswer, setEditedAnswer] = useState({});
 
-  // EDIT ANSWER HANDLERS
+  // Handler for editing an answer
   const editAnswer = (index) => {
     setIsEditing({ ...isEditing, [index]: true });
   };
 
+  // Handler for saving an edited answer
   const saveEditedAnswer = (index) => {
     // Extract the ID and new content of the answer to update
     const answerId = gptArray[index]._id; // Assuming each answer has a unique _id property
@@ -85,27 +82,30 @@ const handleElementClick = (elementId) => {
       });
   };
 
-  // HANDLERS
-
-  //handleChange
+  // Handler for handling input changes
   const handleChange = (e) => {
     setNewQuestion(e.target.value);
     console.log(newQuestion);
   };
 
+  // Handler for handling form submissions
   const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    // Remove the extra forward slash at the end of the base URL
-    const baseUrl = process.env.REACT_APP_API_URL.replace(/\/$/, '');
-    const response = await axios.post(
-      `${process.env.REACT_APP_API_URL}/generate-image`,
-      {}
+    e.preventDefault();
+    try {
+      // Remove the extra forward slash at the end of the base URL
+      const baseUrl = process.env.REACT_APP_API_URL.replace(/\/$/, "");
 
+      // Send a POST request to the backend to generate an image
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/generate-image`,
+        {}
       );
+      // Extract the image URL from the response
       const imageUrl = response.data.data[0].url;
+      // Update the state with the generated image URL
       setGeneratedImageUrl(imageUrl);
 
+      // Send a POST request to the backend to get GPT-3 response
       const gptResponse = await axios.post(
         `${process.env.REACT_APP_API_URL}/gpt`,
         {
@@ -114,18 +114,21 @@ const handleElementClick = (elementId) => {
         }
       );
 
+      // Update the state with the new GPT-3 response
       setGptArray([...gptArray, gptResponse.data]);
+      // Reset the newQuestion and generatedImageUrl states
       setNewQuestion("");
       setGeneratedImageUrl(null);
 
       // Fetch the data again after successfully submitting a new question
       getData();
     } catch (error) {
+      // Handle any errors that occur during the process
       console.error(error);
     }
   };
 
-
+  // Function to handle deleting an answer
   const handleDelete = (answerId) => {
     // Send a DELETE request to the backend with the ID of the entry to be deleted
     axios
@@ -144,7 +147,6 @@ const handleElementClick = (elementId) => {
   // THE getData() Function
   // Retrieves all the data in the DB
   // Fills our Array with Data Objects
-
   const getData = () => {
     axios.get(`${process.env.REACT_APP_API_URL}/gpt`).then((response) => {
       setGptArray(response.data);
@@ -153,10 +155,9 @@ const handleElementClick = (elementId) => {
 
   // USE EFFECT
   // Have GPT Array in the Dependency Array so when it is updated, callAPI() is called and page refreshes!!
-
-useEffect(() => {
-  getData();
-}, [gptArray]);
+  useEffect(() => {
+    getData();
+  }, [gptArray]);
 
   return (
     <div className="App container">
@@ -207,7 +208,6 @@ useEffect(() => {
                           language="javascript"
                           showLineNumbers={true}
                           startingLineNumber={1}
-                          onClick={() => handleElementClick(gpt._id)}
                         />
                         <button onClick={() => editAnswer(i)}>Edit</button>
                       </>
